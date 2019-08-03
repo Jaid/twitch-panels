728b6f7d5d29b4a8d5789ac95f4469f926cb6ec7
Jaid <jaid.jsx@gmail.com>
2019-08-03 05:12:49 +0200

    feat: Added image trimming
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/plugins/main/index.js
+++ src/plugins/main/index.js
@@ -1,4 +1,5 @@
 import puppeteer from "puppeteer"
+import sharp from "sharp"
 
 export default class {
 
@@ -9,10 +10,22 @@ export default class {
     core.koa.use(async context => {
       context.body = require("!raw-loader!twitch-panel-html/index.html").default
     })
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({
+      defaultViewport: {
+        width: 1920,
+        height: 1080,
+        isLandscape: true,
+      },
+    })
     const page = await browser.newPage()
-    await page.goto(`http://localhost:${core.config.insecurePort}`)
-    await page.screenshot({path: "dist/panel.png"})
+    await page.goto(`http://localhost:${core.config.insecurePort}?mode=a`)
+    const buffer = await page.screenshot({
+      omitBackground: true,
+      fullPage: true,
+    })
+    const sharpImage = sharp(buffer)
+    sharpImage.trim()
+    await sharpImage.toFile("dist/panel.png")
   }
 
 }
